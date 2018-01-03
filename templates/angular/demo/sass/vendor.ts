6aed4cbbfef74759ac010f06738b26edd848e7d3
tanbo <464328895@qq.com>
2018-01-03 20:43:04 +0800

    优化ng包大小

--- templates/angular/demo/sass/vendor.ts
+++ templates/angular/demo/sass/vendor.ts
@@ -1,9 +1,7 @@
 // Angular
 import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-import '@angular/http';
 import '@angular/router';
 // RxJS
 import 'rxjs';

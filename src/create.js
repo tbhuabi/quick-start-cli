285bfe3b99a173e92901d62768f9272caa042e7e
tanbo <464328895@qq.com>
2018-04-09 18:29:11 +0800

    vue ts

--- src/create.js
+++ src/create.js
@@ -7,12 +7,12 @@ const line = require('./line');
 const log = require('./logger');
 const cssConfig = require('./css-config.json');
 
-module.exports = function (result) {
+module.exports = function (config) {
     line();
     log(chalk.green('正在创建项目……'));
 
-    const templateSource = path.resolve(__dirname, `../templates/${result.projectType}/template`);
-    const templateTarget = result.projectName;
+    const templateSource = path.resolve(__dirname, `../templates/${config.projectType.toLowerCase()}/template`);
+    const templateTarget = config.projectName;
 
     copy({
         src: templateSource,
@@ -23,15 +23,15 @@ module.exports = function (result) {
         log(chalk.gray('**') + ' ' + message);
     });
 
-    const demoSource = path.resolve(__dirname, `../templates/${result.projectType}/demo/${result.cssLanguage}`);
-    const demoTarget = path.join(result.projectName, 'src');
+    const demoSource = path.resolve(__dirname, `../templates/${config.projectType}/demo/${config.cssLanguage}`);
+    const demoTarget = path.join(config.projectName, 'src');
     copy({
         src: demoSource,
         dest: demoTarget
     }, () => {
-        const fileName = path.join(result.projectName, 'config/css-config.json');
+        const fileName = path.join(config.projectName, 'config/css-config.json');
         const fileContent = JSON.stringify({
-            language: result.cssLanguage
+            language: config.cssLanguage
         }, null, 2);
         fs.writeFile(fileName, fileContent, error => {
             if (error) {
@@ -39,18 +39,18 @@ module.exports = function (result) {
             }
         });
 
-        let packageJson = require(`../templates/${result.projectType}/template/package.json`);
-        let cssLoaderConfig = cssConfig[result.cssLanguage];
+        let packageJson = require(`../templates/${config.projectType}/template/package.json`);
+        let cssLoaderConfig = cssConfig[config.cssLanguage];
 
-        packageJson.name = result.projectName;
-        if (result.version) {
-            packageJson.version = result.version;
+        packageJson.name = config.projectName;
+        if (config.version) {
+            packageJson.version = config.version;
         }
-        if (result.author) {
-            packageJson.author = result.author;
+        if (config.author) {
+            packageJson.author = config.author;
         }
-        if (result.description) {
-            packageJson.description = result.description;
+        if (config.description) {
+            packageJson.description = config.description;
         }
 
 
@@ -59,13 +59,13 @@ module.exports = function (result) {
                 packageJson.devDependencies[key] = cssLoaderConfig[key];
             }
         }
-        let packageFile = path.join(result.projectName, 'package.json');
+        let packageFile = path.join(config.projectName, 'package.json');
         let packageContent = JSON.stringify(packageJson, null, 2);
         fs.writeFile(packageFile, packageContent, error => {
             if (error) {
                 log(chalk.red(error))
             }
-        })
+        });
         log('--项目配置创建完成--');
         log(chalk.green('项目创建完成！'));
     }).on('log', message => {
